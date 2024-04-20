8098f83e777f81cff627d443eb1fb5657089b349
Nguyễn Xuân Huỳnh <113984833+HuXu689@users.noreply.github.com>
2024-04-20 23:12:59 +0700

    Add files via upload

---
+++ EF_DBFirst.sql
@@ -0,0 +1,26 @@
+﻿-- Tạo bảng Khoa
+CREATE DATABASE TruongEF_DBFirst
+GO
+USE TruongEF_DBFirst
+GO
+CREATE TABLE Khoa (
+    KhoaID INT PRIMARY KEY,
+    TenKhoa NVARCHAR(100)
+);
+
+-- Tạo bảng Lop
+CREATE TABLE Lop (
+    LopID INT PRIMARY KEY,
+    TenLop NVARCHAR(100),
+    KhoaID INT,
+    FOREIGN KEY (KhoaID) REFERENCES Khoa(KhoaID)
+);
+
+-- Tạo bảng SinhVien
+CREATE TABLE SinhVien (
+    SinhVienID INT PRIMARY KEY,
+    TenSinhVien NVARCHAR(100),
+    Tuoi INT,
+    LopID INT,
+    FOREIGN KEY (LopID) REFERENCES Lop(LopID)
+);
